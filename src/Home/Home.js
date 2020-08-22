4a9b3321f090803c4acbb399346986a99273bf70
Dania Duany <deduany@gmail.com>
2020-08-22 09:27:17 +0200

    Add description section
    Reported-by: Dania Duany

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import {ProductSlider} from "../Slider/Slider";
-
+import { ProductSlider } from "../Slider/Slider";
 
 export function Home() {
   return (
@@ -10,8 +9,16 @@ export function Home() {
         <span className={styles.slogan}>DAS BESTE VON AfRIKA</span>
         <span className={styles.sloganPartTwo}>DAS BESTE VON DIR</span>
       </div>
-        <ProductSlider />
-
+      <ProductSlider />
+      <div className={styles.description}>
+        <p className={styles.descriptionText}>
+          Spezialist für Haarkunst und Produkte des afrikanischen Marktes.
+        </p>{" "}
+        <br />
+        <p className={styles.descriptionText}>
+          Kommen Sie vorbei und machen Sie sich einen ersten Eindruck über uns.
+        </p>
+      </div>
     </div>
   );
 }
