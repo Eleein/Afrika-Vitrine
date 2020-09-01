52eff967782654e91afa6e8c1cec354ea13846ca
Dania Duany <deduany@gmail.com>
2020-09-01 15:40:33 +0200

    tablet-layout-home-page
    Adjust the size and layout of the bottom text area for
    a tablet-size device display.
    
    Reported-by: Dania Duany

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "./Home/Home";
 import { Header } from "./Header/Header";
-import {Footer} from "./zFooter/Footer";
+import { Footer } from "./zFooter/Footer";
 
 function App() {
   return (
     <div className="App">
-        <div className="content-wrap">
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <div>Whatever, dude</div>
-        </Route>
-      </Switch>
-        </div>
-        <Footer/>
+      <div className="content-wrap">
+        <header>
+          <Header />
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <div>Whatever, dude</div>
+          </Route>
+        </Switch>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
