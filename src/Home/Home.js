afa67ed60f1435e614450d75d1fdabb031f330f9
Dania Duany <deduany@gmail.com>
2020-09-01 15:58:25 +0200

    Refactor the default display to work with flex, because the later displays also depend on flex, and this reduces the code footprint.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -13,8 +13,7 @@ export function Home() {
       <div className={styles.description}>
         <p className={styles.descriptionText}>
           Spezialist für Haarkunst und Produkte des afrikanischen Marktes.
-        </p>{" "}
-        <br />
+        </p>
         <p className={styles.descriptionText}>
           Kommen Sie vorbei und machen Sie sich einen ersten Eindruck über uns.
         </p>
